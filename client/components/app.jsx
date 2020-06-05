3e4de142294cee5f85329f88cba8b51f713d0fa1
Marc Nicdao <nicdaom466@gmail.com>
2020-06-05 16:47:15 -0700

    aded GradeTable Component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,9 +8,35 @@ class Header extends React.Component {
   }
 }
 
+class GradeTable extends React.Component {
+  render() {
+    const grades = this.props.grades ? '' : 'No grades recorded';
+    return (
+      <table className="table table-striped">
+        <thead className="thead-dark">
+          <tr>
+            <th>Student Name</th>
+            <th>Course</th>
+            <th>Grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {grades}
+        </tbody>
+      </table>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <Header/>;
+    return (
+      <div>
+        <Header />
+        <GradeTable grades=''/>
+      </div>
+    );
+
   }
 }
 
