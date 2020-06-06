f6e8e968bc0a9bdb433fa99538f573a961762c87
Marc Nicdao <nicdaom466@gmail.com>
2020-06-05 17:26:08 -0700

    Feature 1 of sgt Complete

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,32 +8,68 @@ class Header extends React.Component {
   }
 }
 
+class Grade extends React.Component {
+
+  render() {
+    const { grade, name, course } = this.props;
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{course}</td>
+        <td>{grade}</td>
+      </tr>
+    );
+  }
+}
+
 class GradeTable extends React.Component {
   render() {
-    const grades = this.props.grades ? '' : 'No grades recorded';
+    const { grades } = this.props;
+    const row = grades.length > 0
+      ? grades.map(grade => {
+        return <Grade key={grade.id} name={grade.name} course={grade.course} grade={grade.grade}/>;
+      })
+      : 'No grades recorded';
     return (
-      <table className="table table-striped">
-        <thead className="thead-dark">
-          <tr>
-            <th>Student Name</th>
-            <th>Course</th>
-            <th>Grade</th>
-          </tr>
-        </thead>
-        <tbody>
-          {grades}
-        </tbody>
-      </table>
+      <div>
+        <table className="table table-striped">
+          <thead className="thead-dark">
+            <tr>
+              <th>Student Name</th>
+              <th>Course</th>
+              <th>Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            {row}
+          </tbody>
+        </table>
+
+      </div>
     );
   }
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grades: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('api/grades')
+      .then(data => data.json())
+      .then(grades => this.setState({ grades: grades }));
+  }
+
   render() {
+    const { grades } = this.state;
     return (
       <div>
         <Header />
-        <GradeTable grades=''/>
+        <GradeTable grades={grades}/>
       </div>
     );
 
