4c829da775beb8c81f33ce07b20252579cb3fc0e
Marc Nicdao <nicdaom466@gmail.com>
2020-06-07 15:04:55 -0700

    completed average grade feature

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -11,26 +11,24 @@ class App extends React.Component {
   }
 
   getAverage() {
-    const grades = this.state.grades.filter(el => true);
-    console.log(grades);
-    const totalPoints = grades.reduce((item1, item2) => {
-      return parseInt(item1.grade) + parseInt(item2.grade);
+    const grades = this.state.grades.map(el => el.grade);
+    const totalPoints = grades.reduce((n1, n2) => {
+      return Number(n1) + Number(n2);
     }, 0);
-    return totalPoints / this.state.grades.length;
+    return totalPoints / grades.length;
   }
 
   componentDidMount() {
     fetch('api/grades')
       .then(data => data.json())
-      .then(grades => this.setState({ grades: grades }))
-      .catch(error => console.error(error));
+      .then(grades => this.setState({ grades: grades }));
   }
 
   render() {
     const { grades } = this.state;
     return (
       <div>
-        <Header average={this.getAverage()} totalPoints={this.totalPoints}/>
+        <Header average={this.getAverage().toString()}/>
         <GradeTable grades={grades} />
       </div>
     );
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default class Header extends React.Component {
   componentDidMount() {
-    console.log(this.props);
   }
 
   render() {
@@ -10,7 +9,7 @@ export default class Header extends React.Component {
     return (
       <div className='header'>
         <h1 className='title'>Student Grade Table</h1>
-        <h3>Average Grade <span>{average}</span></h3>
+        <h3>Average Grade: <span>{average}</span></h3>
       </div>
     );
   }
